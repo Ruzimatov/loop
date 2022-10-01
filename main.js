f26afdad62ae45cea71670a2186076418d06e918
Ruzimatov <rustamruzimatov92@gmail.com>
2022-10-01 13:15:39 +0500

    hometask3

---
+++ main.js
@@ -0,0 +1,65 @@
+// 1
+// let k = +prompt('k =');
+// let n = +prompt('n =');
+//  for(let i=1; i<=n; i++){
+//     console.log(k);
+//  }
+
+//  2
+//  let a = +prompt('a=');
+//  let b = +prompt('b=');
+
+//  for(let i=a; i<=b; i++){
+//     console.log(i);
+//  }
+//  2
+//  let a = +prompt('a=');
+//  let b = +prompt('b=');
+
+//  for(let i=b; i>=a; i--){
+//      console.log(i);
+//  }
+
+// 3
+// let kg = 10000;
+// for(let i=1; i<=10; i++){
+//    console.log(i*kg);
+// }
+
+// 4
+// let kg = 10000;
+// for(let i=1; i<=10; i++){
+//    console.log(((i/10)*kg));
+// }
+// 5
+// let kg = 10000;
+// for(let i=12; i<=20; i++){
+//     console.log((i/10)*kg);
+// }
+
+// 6
+// let a = +prompt('a =');
+// let b = +prompt('b =');
+// for( let i=a; i<b; i++){
+//     let sum = 0;
+//     sum+=i;
+// }  ?????
+
+// 14
+// let son = +prompt('n son kiriting');
+
+// for(let i=0; i<=son; i++){
+//     console.log(i**2);
+// }
+
+// 15 
+// let son = +prompt('n son kiriting');
+// for(let i=0; i>=son; i--){
+//     console.log(i);
+// } ???
+
+// 16 
+// let son = +prompt('n son kiriting');
+// for(let i=0; i<=son; i++){
+//     console.log(i/5 ==0);
+// }
